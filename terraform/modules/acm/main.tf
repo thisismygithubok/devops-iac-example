b28b40381179d620fd4c863cb19deabbf88b0fae
Steven Schmidt <steven@thisismynameok.com>
2025-09-23 09:50:27 +1000

    Fix ACM private key resource name

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -1,14 +1,14 @@
 ### Certificate Resources ###
 
 # Generate Private Key
-resource "tls_private_key" "private_key_ed25519" {
+resource "tls_private_key" "private_key" {
     algorithm = "RSA"
     rsa_bits = 2048
 }
 
 # Generate Self-Signed Cert
 resource "tls_self_signed_cert" "self_signed_cert" {
-    private_key_pem = tls_private_key.private_key_ed25519.private_key_pem
+    private_key_pem = tls_private_key.private_key.private_key_pem
     validity_period_hours = 8760 # 1 year
     allowed_uses = [
         "key_encipherment",
@@ -24,6 +24,6 @@ resource "tls_self_signed_cert" "self_signed_cert" {
 
 # ACM - Import Self-Signed Cert
 resource "aws_acm_certificate" "self_signed" {
-    private_key = tls_private_key.private_key_ed25519.private_key_pem
+    private_key = tls_private_key.private_key.private_key_pem
     certificate_body = tls_self_signed_cert.self_signed_cert.cert_pem
 }
